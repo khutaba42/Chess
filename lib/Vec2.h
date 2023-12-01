7a22e48b4b765e7d6bc958ca74bb71a3778111ce
khutaba42 <khutaba.42@gmail.com>
2023-12-01 23:06:32 +0200

    added some functionality to the Vec2 clsss lke the + - += -+ operatots

--- lib/Vec2.h
+++ lib/Vec2.h
@@ -18,4 +18,18 @@ struct Vec2
     Vec2(T x, T y) : x(x), y(y) {}
     friend bool operator==(const Vec2 a, const Vec2 b) { return (a.x == b.x) && (a.y == b.y); }
     friend bool operator!=(const Vec2 a, const Vec2 b) { return !(a == b); }
+    friend Vec2 operator+(const Vec2 a, const Vec2 b) { return Vec2(a.x + b.x, a.y + b.y); }
+    Vec2 &operator+=(const Vec2 other)
+    {
+        this->x += other.x;
+        this->y += other.y;
+        return *this;
+    }
+    friend Vec2 operator-(const Vec2 a, const Vec2 b) { return Vec2(a.x - b.x, a.y - b.y); }
+    Vec2 &operator-=(const Vec2 other)
+    {
+        this->x -= other.x;
+        this->y -= other.y;
+        return *this;
+    }
 };
